84d828ed031f8dbc06a9c891bfba3e4266eb36ad
Sabrinugh <sabrina.boc@outlook.com>
2026-03-12 19:15:44 +0000

    Created project with SpringBoot Intializr for events manager

---
+++ eventsmanager/src/test/java/com/example/eventsmanager/EventsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
